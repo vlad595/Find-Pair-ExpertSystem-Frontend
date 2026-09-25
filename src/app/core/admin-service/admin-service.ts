b46eabbcbe6dfc2a7840e2b20e8988edaa16b1e9
vlad595 <vladggaort@gmail.com>
2026-09-25 15:52:40 +0300

    Added admin service

--- src/app/core/admin-service/admin-service.ts
+++ src/app/core/admin-service/admin-service.ts
@@ -1,4 +1,39 @@
-import { Service } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Service } from '@angular/core';
+import { CreateRule, DeleteRule } from '../models/rule.model';
+import { BehaviorSubject, map, tap } from 'rxjs';
+import { Rule } from '../models/rule.model';
 
 @Service()
-export class AdminService {}
+export class AdminService {
+    private readonly apiUrl = 'http://localhost:5113'
+    private readonly http = inject(HttpClient);
+
+    rulesSubject = new BehaviorSubject<Rule[]>([]);
+    $rules = this.rulesSubject.asObservable();
+
+    getRules(){
+        return this.http.get<Rule[]>(this.apiUrl + '/api/Rules').pipe(
+            tap(response => {
+                this.rulesSubject.next(response);
+                console.log('Rules are pulled successfully!', this.rulesSubject.value);
+            })
+        );
+    }
+    postRule(rule: CreateRule){
+        return this.http.post<Rule>(this.apiUrl + '/api/Rules', rule).pipe(
+            tap(response => {
+                this.rulesSubject.next([response, ...this.rulesSubject.value]);
+                console.log('Rule are created successfully!', this.rulesSubject.value);
+            })
+        );
+    }
+    deleteRule(ruleId: DeleteRule){
+        return this.http.delete(this.apiUrl + '/api/Rules' + ruleId.Id).pipe(
+            tap(respone => {
+                this.rulesSubject.next([...this.rulesSubject.value.filter(r => r.Id !== ruleId.Id)]);
+                console.log('Rule are deleted successfully!', this.rulesSubject.value);
+            })
+        );
+    }
+}
--- src/app/core/models/rule.model.ts
+++ src/app/core/models/rule.model.ts
@@ -6,3 +6,15 @@ export interface Rule{
     ResultFact: string;
     ResultValue: string;
 }
+
+export interface CreateRule{
+    Description: string;
+    ConditionFact: string;
+    ConditionValue: string;
+    ResultFact: string;
+    ResultValue: string;
+}
+
+export interface DeleteRule{
+    Id: string;
+}
